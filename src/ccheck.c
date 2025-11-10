9d6145a40485cfc5ecff1d541e48412347f3203a
SacredDever <sacreddever@gmail.com>
2025-11-10 15:22:40 -0500

    init: first commit

---
+++ src/ccheck.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+#include "ccheck.h"
+#include "debug.h"
+
+/*
+ * Options (see the assignment document for details):
+ *   -w           play white
+ *   -b           play black
+ *   -r           randomized play
+ *   -v           give info about search
+ *   -d           don't try to use X window system display
+ *   -t           tournament mode
+ *   -a <num>     set average time per move (in seconds)
+ *   -i <file>    initialize from saved game score
+ *   -o <file>    specify transcript file name
+ */
+
+int ccheck(int argc, char *argv[])
+{
+    // TO BE IMPLEMENTED
+    fprintf(stderr, "The game will not work until you implement this function.\n");
+    abort();
+}
---
+++ src/main.c
@@ -0,0 +1,13 @@
+/**
+ * === DO NOT MODIFY THIS FILE ===
+ * === DO NOT MODIFY THIS FILE ===
+ */
+
+#include <stdlib.h>
+
+#include "ccheck.h"
+
+int main(int argc, char *argv[]) {
+    return ccheck(argc, argv);
+}
+
